84cf4506cc13e5fbad3f5b10aaef151e18abb53f
dute0987 <dute0987@gmail.com>
2022-02-24 18:17:31 +0530

    Main file for Emp present or absent

---
+++ UC-1-EmpPreAbs .cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_8_EmplyoeeWageProblemUsingOOps
+{
+    public class UC_1_EmpPreAbs
+    {
+        public void EmpPresentAbsent()
+        {
+            Random random = new Random();
+            int num = random.Next(2);
+
+            if (num == 1)
+            {
+                Console.WriteLine("Emplyoee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Emplyoee is Absent");
+            }
+        }
+    }
+}
