b84167a8adae0e1d69ae36cc40f3d1f01d605804
dute0987 <dute0987@gmail.com>
2022-02-24 18:30:45 +0530

    Main file for Wage of Month

---
+++ UC-5-MonthWageCal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_8_EmplyoeeWageProblemUsingOOps
+{
+    public class UC_5_MonthWageCal
+    {
+        public void CalculationOfMonthlyWage()
+        {
+            const int IsPartTime = 1;
+            const int FullTime = 2;
+            int WageRatePerHour = 20;
+            int EmpWage = 0;
+            int EmpHour = 0;
+            int TotalSalary = 0;
+            int NumberOfWorkingDay = 20;
+
+            for (int i = 1; i <= NumberOfWorkingDay; i++)
+            {
+                Random random = new Random();
+                int num = random.Next(3);
+
+                if (num == IsPartTime)
+                {
+                    EmpHour = 8;
+                }
+                else if (num == FullTime)
+                {
+                    EmpHour = 12;
+                }
+                else
+                {
+                    EmpHour = 0;
+                }
+                EmpWage = (WageRatePerHour * EmpHour);
+                TotalSalary = (TotalSalary + EmpWage);
+                Console.WriteLine("The Total salary for the month is :- " + TotalSalary);
+            }
+        }
+    }
+}
