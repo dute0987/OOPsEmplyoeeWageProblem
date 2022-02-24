1171e56066ff07fdbda6672ca703921d9d2abc10
dute0987 <dute0987@gmail.com>
2022-02-24 18:20:59 +0530

    main file for Emp Daily wage

---
+++ UC-2-EmpWageDaily.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_8_EmplyoeeWageProblemUsingOOps
+{
+    public class UC_2_EmpWageDaily
+    {
+        public void CalDailyEmpWage()
+        {
+            int WageRatePerHour = 20;
+            int FullDayHour = 8;
+            int CalEmpWage = 0;
+
+            Random random = new Random();
+            int num = random.Next(2);
+
+            if (num == 1)
+            {
+                CalEmpWage = WageRatePerHour * FullDayHour;
+
+                Console.WriteLine("Wage Calculation is:- " + CalEmpWage);
+            }
+            else
+            {
+                CalEmpWage = 0;
+                Console.WriteLine("Wage Calculation is:- " + CalEmpWage);
+            }
+        }
+    }
+}
