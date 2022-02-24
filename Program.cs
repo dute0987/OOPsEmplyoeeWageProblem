74f09608aa828e1ba8dbcc75d07d277ab0e25694
dute0987 <dute0987@gmail.com>
2022-02-24 18:27:10 +0530

    Program.cs for case stament

--- Program.cs
+++ Program.cs
@@ -16,13 +16,13 @@ namespace Day_8_EmplyoeeWageProblemUsingOOps
             //UC_2_EmpWageDaily UC2 = new UC_2_EmpWageDaily();
             //UC2.CalDailyEmpWage();
 
-            //UC-3-Problem
-            UC_3_PartFullTime UC3 = new UC_3_PartFullTime();
-            UC3.WageOfParTimeFullTime();
+            ////UC-3-Problem
+            //UC_3_PartFullTime UC3 = new UC_3_PartFullTime();
+            //UC3.WageOfParTimeFullTime();
 
-            ////UC-4-Problem
-            //UC_4_SwitchCaseProblem UC4 = new UC_4_SwitchCaseProblem();
-            //UC4.SwitchStatmentCaseProblem();
+            //UC-4-Problem
+            UC_4_SwitchCaseProblem UC4 = new UC_4_SwitchCaseProblem();
+            UC4.SwitchStatmentCaseProblem();
 
             ////UC-5-Problem
             //UC_5_MonthWageCal UC5 = new UC_5_MonthWageCal();
